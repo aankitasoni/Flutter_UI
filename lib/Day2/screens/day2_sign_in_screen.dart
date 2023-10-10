40f5b72f678e6e1c8b6f50288b3a76fbdf86e962
Ankita Soni <ankitasoni018@gmail.com>
2023-10-10 23:49:08 +0530

    Updated Day 2 Sign In Screen

--- lib/Day2/screens/day2_sign_in_screen.dart
+++ lib/Day2/screens/day2_sign_in_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/Day2/screens/day2_content_screen.dart';
 
 class Day2SignInScreen extends StatelessWidget {
   const Day2SignInScreen({super.key});
@@ -6,7 +7,19 @@ class Day2SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Sign In'),),
+      appBar: AppBar(
+        title: const Text(
+          'Sign In',
+          style: TextStyle(
+            color: Colors.purple,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            fontFamily: 'Serif',
+          ),
+          // textAlign: TextAlign.center,
+        ),
+      ),
+      body: Body(),
     );
   }
 }

c0c06ade70b2f9079caa8866d3ebe0fb47ebb0b3
Ankita Soni <ankitasoni018@gmail.com>
2023-11-08 23:30:35 +0530

    Updated screen

---
+++ lib/Day7&8_Coffee_Shop_App/main_screen/day7&8_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Day7And8HomeScreen extends StatelessWidget {
+  const Day7And8HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenges/Day3_Plant_App/main_screen/day3_home_screen.dart';
 import 'package:flutter_ui_challenges/Day6_E-Commerce_App/main_screen/Day6_home_screen.dart';
+import 'package:flutter_ui_challenges/Day7&8_Coffee_Shop_App/main_screen/day7&8_home_screen.dart';
 import '../Day1_Onboarding_Screen/screens/day1_home_screen.dart';
 import 'package:flutter_ui_challenges/Day2_Login_SignUp_Screen/main_screen/day2_home_screen.dart';
 import '../Day4&5_Furniture_App/screens/day4&5_home_screen.dart';
@@ -19,8 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
     'Day 3',
     'Day 4 & 5', //d
     'Day 6',
-    'Day 7',
-    'Day 8',
+    'Day 7 & 8',
     'Day 9',
     'Day 10',
     'Day 11',
@@ -51,6 +51,7 @@ class _HomeScreenState extends State<HomeScreen> {
     const Day3HomeScreen(),
     const Day4And5HomeScreen(),
     const Day6HomeScreen(),
+    const Day7And8HomeScreen(),
   ];
 
   @override

7e9759662394d6b362edc97237fb9f2a181caed2
Ankita Soni <ankitasoni018@gmail.com>
2023-11-29 15:01:33 +0530

    Updated screen

--- lib/Day6&7_Coffee_Shop_App/screens/detail_screen.dart
+++ lib/Day6&7_Coffee_Shop_App/screens/detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenges/Day6&7_Coffee_Shop_App/widgets/bottom_bar.dart';
+
+import '../widgets/item_widget.dart';
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key});
@@ -33,27 +36,128 @@ class _DetailScreenState extends State<DetailScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF212325),
       body: SafeArea(
-        child: ListView(children: [
-          Padding(
-            padding: EdgeInsets.only(
-              top: 15,
+        child: ListView(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: 15,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.sort_rounded,
+                      color: Colors.white.withOpacity(0.5),
+                      size: 35,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.notifications,
+                      color: Colors.white.withOpacity(0.5),
+                      size: 35,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                InkWell(
-                  onTap: () {},
-                  child: Icon(
-                    Icons.sort_rounded,
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: Text(
+                "It's a Great Day for Coffee",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 20,
+              ),
+              width: MediaQuery.of(context).size.width,
+              height: 60,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 50, 54, 56),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'Find your Coffee',
+                  hintStyle: TextStyle(
                     color: Colors.white.withOpacity(0.5),
                   ),
-                )
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 30,
+                    color: Colors.white.withOpacity(0.5),
+                  ),
+                ),
+              ),
+            ),
+            TabBar(
+              controller: _tabController,
+              labelColor: Color(0xFFE57734),
+              unselectedLabelColor: Colors.white.withOpacity(0.5),
+              isScrollable: true,
+              indicator: UnderlineTabIndicator(
+                borderSide: BorderSide(
+                  width: 3,
+                  color: Color(0xFFE57734),
+                ),
+                insets: EdgeInsets.symmetric(
+                  horizontal: 16,
+                ),
+              ),
+              labelStyle: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+              labelPadding: EdgeInsets.symmetric(horizontal: 20),
+              tabs: [
+                Tab(
+                  text: 'Hot Coffee',
+                ),
+                Tab(
+                  text: 'Cold Coffee',
+                ),
+                Tab(
+                  text: 'Cappuccino',
+                ),
+                Tab(
+                  text: 'Americano',
+                ),
               ],
             ),
-          ),
-        ]),
+            SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: [
+                ItemsWidget(),
+                ItemsWidget(),
+                ItemsWidget(),
+                ItemsWidget(),
+              ][_tabController.index],
+            ),
+          ],
+        ),
       ),
+      bottomNavigationBar: BottomBar(),
     );
   }
 }
